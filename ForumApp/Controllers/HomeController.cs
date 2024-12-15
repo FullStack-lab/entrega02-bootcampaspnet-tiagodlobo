c1aa344403c445f76eeccf81db370ff1817076ae
Tiago Lobo <tiago.fav@gmail.com>
2024-12-15 17:21:55 -0300

    GET e index

--- ForumApp/Controllers/HomeController.cs
+++ ForumApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ForumApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,24 +7,31 @@ using System.Web.Mvc;
 
 namespace ForumApp.Controllers
 {
-    public class HomeController : Controller
+    public class ComentariosController : Controller
     {
+        private static List<Comentario> _comentarios = new List<Comentario>();
+        private static int _nextId = 1;
+
+        // GET: Comentarios
         public ActionResult Index()
         {
-            return View();
+            var comentariosPrincipais = _comentarios.Where(c => c.ComentarioId == null).ToList();
+            return View(comentariosPrincipais);
         }
 
-        public ActionResult About()
+        // GET: Comentarios/Details
+        public ActionResult Details(int id)
         {
-            ViewBag.Message = "Your application description page.";
+            var comentario = _comentarios.FirstOrDefault(c => c.Id == id);
+            if (comentario == null)
+                return HttpNotFound();
 
-            return View();
+            return View(comentario);
         }
 
-        public ActionResult Contact()
+        // GET: Comentarios/Create
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
---
+++ ForumApp/Models/Comentario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ForumApp.Models
+{
+    public class Comentario
+    {
+        public int Id { get; set; }
+        public string Texto { get; set; }
+        public string Autor { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public int? ComentarioId { get; set; }
+        public virtual Comentario ComentarioPrincipal { get; set; }
+        public virtual ICollection<Comentario> Respostas { get; set; }
+    }
+}
